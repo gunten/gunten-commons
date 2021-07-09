e0195dcdfb89f4329f830dba07d9ed3bcbab7bc3
twoplus <mm_8023@hotmail.com>
2021-07-09 10:56:44 +0800

    去除无效代码

--- tp-activiti6-springboot-demo/src/main/java/org/tp/Activiti6DemoApplication.java
+++ tp-activiti6-springboot-demo/src/main/java/org/tp/Activiti6DemoApplication.java
@@ -5,7 +5,8 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 /**
- * @author
+ * springboot2.0不能与activiti6.0.0直接集成使用，因为activiti6.0.0出来的时候springboot2.0还没有出来，
+ * activiti6.0.0 支持springboot1.2.6以上，2.0.0以下的版本。
  */
 @SpringBootApplication(exclude = SecurityAutoConfiguration.class)
 public class Activiti6DemoApplication {
--- tp-spring-batch/src/main/java/org/tp/BatchApplication.java
+++ tp-spring-batch/src/main/java/org/tp/BatchApplication.java
@@ -1,17 +1,13 @@
 package org.tp;
 
-import org.activiti.spring.boot.SecurityAutoConfiguration;
 import org.mybatis.spring.annotation.MapperScan;
 import org.springframework.batch.core.configuration.annotation.EnableBatchProcessing;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.context.annotation.EnableAspectJAutoProxy;
 
-/**
- * springboot2.0不能与activiti6.0.0直接集成使用，因为activiti6.0.0出来的时候springboot2.0还没有出来，
- * activiti6.0.0 支持springboot1.2.6以上，2.0.0以下的版本。
- */
-@SpringBootApplication(scanBasePackages = "org.tp", exclude = SecurityAutoConfiguration.class)
+
+@SpringBootApplication(scanBasePackages = "org.tp")
 @MapperScan("org.tp.transactional.annotation.mapper")
 @EnableBatchProcessing
 @EnableAspectJAutoProxy(exposeProxy = true, proxyTargetClass = true)
